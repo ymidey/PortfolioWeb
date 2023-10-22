aa37c67fae868a60c1cec640572298a5cc144510
ymidey <midey.yannick@gmail.com>
2023-10-22 22:22:11 +0200

    Add style for navbar

--- transition.js
+++ transition.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const myTitle = new SplitType('#hero-title')
+    const myParagraphe = new SplitType('#hero-paragraphe')
+
 
     gsap.to('.char', {
         y: 0,
@@ -8,6 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
         duration: .1
     })
 
+    gsap.to('.char', {
+        y: 0,
+        stagger: 0.03,
+        delay: 0.2,
+        duration: .1
+    })
 
 
 })
