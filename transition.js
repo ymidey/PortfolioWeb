724ac512bd06292dc9e55598e510ef063b51fa59
ymidey <midey.yannick@gmail.com>
2023-10-25 23:08:14 +0200

    Add headings for sections

--- transition.js
+++ transition.js
@@ -5,16 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     gsap.to('.char', {
         y: 0,
         stagger: 0.05,
-        delay: 0.2,
+        delay: 0.3,
         duration: .1
     })
 
-    gsap.to('.char', {
-        y: 0,
-        stagger: 0.03,
-        delay: 0.2,
-        duration: .1
-    })
 
 
 })
