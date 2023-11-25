25aa01bb6960ea9fc791594f1a72dc634eb05eed
ymidey <midey.yannick@gmail.com>
2023-11-25 23:05:16 +0100

    Add svg background and gsap animation

--- transition.js
+++ transition.js
@@ -2,13 +2,61 @@ document.addEventListener("DOMContentLoaded", function () {
     const myTitleHeader = new SplitType('#hero-title')
     const myParagrapheHeader = new SplitType('#hero-paragraphe')
 
+    gsap.registerPlugin(ScrollTrigger);
+
     gsap.to('.char', {
         y: 0,
         stagger: 0.04,
-        delay: 0.1,
         duration: .1
     })
 
+    const skills = document.querySelectorAll(".skills li")
+    function animationSkills() {
+        gsap.from(skills, {
+            opacity: 0,
+            y: 100,
+            stagger: 0.2, // délai entre chaque élément
+            duration: .7, // durée totale de l'animation
+        });
+    }
 
+    animationSkills();
 
+    // Animation déclenchée à l'arrivée sur l'élément
+    ScrollTrigger.create({
+        trigger: skills,
+        start: "bottom",
+        onEnter: animationSkills,
+    });
+
+    const lineLeft = document.querySelector(".line-left");
+    const lineHero = document.querySelector(".line-hero");
+    const lineIcons = document.querySelectorAll(".line-icons a");
+
+    // Fonction pour animer les éléments
+    function animateElements() {
+        gsap.from(lineHero, {
+            height: 0,
+            opacity: 0,
+            y: 100,
+            duration: 1,
+        });
+
+        gsap.from(lineIcons, {
+            opacity: 0,
+            y: 100,
+            stagger: 0.2, // Délai entre chaque élément
+            duration: 1,
+        });
+    }
+
+    // Lancer l'animation au chargement de la page
+    animateElements();
+
+    // Animation déclenchée lorsqu'on arrive sur la section (.line-left)
+    ScrollTrigger.create({
+        trigger: lineLeft,
+        start: "bottom",
+        onEnter: animateElements,
+    });
 })
